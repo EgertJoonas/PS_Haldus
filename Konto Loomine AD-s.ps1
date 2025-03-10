0be8a3443dc3be74b8e6449b7392a7dad849a8a3
Egert Joonas <egert.joonas@voco.ee>
2025-03-10 08:20:02 +0000

    Active Directory konto loomine

---
+++ Konto Loomine AD-s.ps1
@@ -0,0 +1,19 @@
+﻿#Küsib kasutajalt eesnime ja perenime
+
+$Eesnimi = Read-Host "Sisestage eesnimi"
+$Perenimi = Read-Host "Sisestage perenimi"
+
+#liidab nime
+$kasutaja = "$($eesnimi.ToLower()).$($perenimi.ToLower())"
+#kontrollib kas kasutaja on olemas
+$kasutaja2 = Get-ADUser -Filter {SamAccountName -eq $kasutaja}
+
+if ($kasutaja2) {
+    Write-Host "Kasutaja $Eesnimi $Perenimi on juba olemas."
+} else {
+#loob uue konto
+$Parool = ConvertTo-SecureString "qwerty" -AsPlainText -Force
+New-ADUser -SamAccountName $kasutaja -UserPrincipalName "$kasutaja@domain.local" -Name ($Eesnimi $Perenimi) -GivenName $Eesnimi -Surname $Perenimi -AccountPassword $Parool -Enabled $true -Path "CN=Users,DC=domain,DC=local"
+
+Write-Host "Kasutaja on loodud."
+}
