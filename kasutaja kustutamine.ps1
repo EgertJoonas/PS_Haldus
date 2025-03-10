72d2ae2eadb38b482c342336a5092ab4825d347a
Egert Joonas <egert.joonas@voco.ee>
2025-03-10 08:09:28 +0000

    Kasutaja kustutamine

---
+++ kasutaja kustutamine.ps1
@@ -0,0 +1,16 @@
+﻿#küsib kasutajalt eesnime ja perenime
+$eesnimi = Read-Host "Sisesta oma eesnimi:"
+$perenimi = Read-Host "Sisesta oma perenimi:"
+
+#liidab nimed kokku
+$kasutaja = "$($eesnimi.ToLower()).$($perenimi.ToLower())"
+
+#proovib kustutada kontot
+try{
+    Remove-LocalUser -Name $kasutaja
+    Write-Host "Kasutaja $kasutaja on kustutatud"
+}
+#väljastab veateate, kui ei suutnud ära kustutada
+catch{
+      Write-Host "Kasutaja kustutamisega tekkis probleem" -ForegroundColor DarkRed
+}
