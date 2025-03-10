bc59f7284d7fb0bbdc5e4a5bdab8928c644ec21f
Egert Joonas <egert.joonas@voco.ee>
2025-03-10 10:50:18 +0000

    Konto kustutamine Active Directorys

---
+++ Konto kustutamine AD-s.ps1
@@ -0,0 +1,15 @@
+﻿#Küsib andmeid
+$Eesnimi = Read-Host "Sisestage kasutaja eesnimi"
+$Perenimi = Read-Host "Sisestage kasutaja perenimi"
+
+#Liidab nimed
+$kasutaja = $($Eesnimi.ToLower()).$($Perenimi.ToLower())
+#Otsib kasutajat
+$User = Get-ADUser -Filter {SamAccountName -eq $kasutaja}
+#Proovib kustutada
+if ($User) {
+    Remove-ADUser -Identity $User
+    Write-Host "Kasutaja on kustutatud."
+} else {
+    Write-Host "Kasutajat ei leitud."
+}
